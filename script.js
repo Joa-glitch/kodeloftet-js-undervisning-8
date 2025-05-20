2f782e1815ef08cbb3eb8e2f6bb7b401111ae801
Joa-glitch <joachim.liseth@gmail.com>
2025-05-20 11:31:48 +0200

    js

--- script.js
+++ script.js
@@ -55,20 +55,34 @@ function band() {
 //
 //
 
+/* Setter opp en funksjon med navnet clickButton som tar en blokk med kode som skal gjøres samtidig */
+
     function clickButton() {
+
+        /* Lager en variabel med navnet h1Element som henter et element fra HTML med id-navn changeText */
       const h1Element = document.getElementById("changeText"); 
       // document.querySelector("#changeText") 
+
+      /* Logger variabelen h1Element til console for å sjekke at vi henter riktig HTML element */
       console.log(h1Element); 
     
     
-
+      /*Lager en variabel med navnet newText som lagrer en string med tekst */
     const newText = "Velkommen til min side";
-
+      /* Variablelen h1Element får tekst innhold fra variabelen newText */
     h1Element.textContent = newText;
     }
     
-
+     /* Lager en variabel med navnet buttonElement som henter et element fra HTML med id-navn button, og logger buttonElement til console for å se om det er riktig */
     const buttonElement = document.getElementById("button");
     console.log(buttonElement);
 
+    /* Bruker variabelen buttonElement for å legge til en hendelseslytter som lytter etter et klikk for å kjøre funksjonen clickButton */
     buttonElement.addEventListener("click", clickButton);
+
+    /*function ?(){
+        const ? = document.getElementById("?");
+        const ? = "?";
+
+        ?.textContent = ?;
+    }*/
