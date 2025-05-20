2f308c15ec9ab3ba4390f12482a79dcaef8e4bf8
Joa-glitch <joachim.liseth@gmail.com>
2025-05-20 10:38:20 +0200

    js

--- script.js
+++ script.js
@@ -51,3 +51,19 @@ function band() {
     const bandName = "Meshuggah";
     const bandRole = `Gitarist`
 }
+
+//
+//
+
+    function clickButton() {
+      const h1Element = document.getElementById("changeText"); 
+      // document.querySelector("#changeText") 
+      console.log(h1Element); 
+    
+    
+
+    const newText = "Velkommen til min side";
+
+    h1Element.textContent = newText;
+}
+    clickButton();
